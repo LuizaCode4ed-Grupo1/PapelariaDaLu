8025cc841bbf10e5dacb6b56b78d1c0c45d174f8
beatrizmakowski <bemakow@gmail.com>
2022-05-22 16:00:37 -0300

    fix(server.js): adicionando import routes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import routes from './src/routes'
 
 const app = express()
 
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next) => res.send('Agora foi'))
+app.use('/', routes)
 
 app.listen(3000, () => {
     console.log('Executando na porta 3000')
