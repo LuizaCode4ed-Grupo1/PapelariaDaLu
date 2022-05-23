bc82787c55006774e274504bbb6af71a926d7cc0
beatrizmakowski <bemakow@gmail.com>
2022-05-23 11:18:21 -0300

    feat(produtos): adicionando produtos-service

---
+++ src/produtos/produtos-service.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+import config from '../config'
+
+mongoose.connect(config.connectionString)
+
+import Produto from './produtos-model'
+
+class ProdutoService {
+
+    cadastrarProduto(produto) {
+        console.log('Inserindo novo produto no mongodb....')
+        const novoProduto = new Produto(produto)
+        return novoProduto.save()
+    }
+
+    listarProdutos() {
+        return Produto.find()
+    }
+    
+}
